71aa7424268123bcf4edad72c27f17142ff956ea
KairuDagaang <kyledagaang1@gmail.com>
2024-09-04 08:32:09 +0800

    first commit

---
+++ src/constants/common.ts
@@ -0,0 +1,15 @@
+export const NAME = 'Kyle Christian G. Daga-ang';
+
+// ABOUT PAGE
+export const ABOUT_TEXT_1 =
+  'A Graduating student of Mater Dei College taking the course BACHELOR OF SCIENCE IN INFORMATION TECHNOLOGY (BSIT) and is currently studying the course subjects that might relate to future jobs.';
+export const ABOUT_TEXT_2 =
+  'New to computer languages but is eager to learn more and adapt to the technological changes to catch up to the professionals and someday become one and make more proud achievements to present to my parents.';
+export const ABOUT_TEXT_3 =
+  'Versatile novice proficient in frontend development, capable of supporting and learning a complete web applications from start to finish. Possess a mild but growing understanding of various programming languages, frameworks, and databases.';
+
+// RESUME PAGE
+export const RESUME_TEXT_1 =
+  'IT Student with 4 years of experience building websites and web applications for school purposes and uses different frameworks. Surface knowledge of front-end technologies and database  management.';
+export const RESUME_TEXT_2 =
+  'IT Student with 4 years of experience building websites and web applications for school purposes and uses different frameworks.';
